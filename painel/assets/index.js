49b05e647b79cc1a002b492524d5a5ab8d8b722f
JonathanJuniorVasconcelos <juniorjonathan@outlook.com>
2025-07-02 19:59:59 -0300

    Create index.js

---
+++ painel/assets/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function Painel() {
+  return React.createElement("h1", null, "Painel OrçaFácil carregado com sucesso!");
+}
+
+createRoot(document.getElementById("root")).render(React.createElement(Painel));
