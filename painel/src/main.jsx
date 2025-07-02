aafdd92eb37c6962a6124807ebce71bfdee2f733
JonathanJuniorVasconcelos <juniorjonathan@outlook.com>
2025-07-02 20:29:44 -0300

    Add files via upload

---
+++ painel/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Painel OrçaFácil carregado com sucesso!</h1>
+  </React.StrictMode>,
+)
