a331f6ac651eeb253443d351340c53066be704a4
JonathanJuniorVasconcelos <juniorjonathan@outlook.com>
2025-07-02 19:45:31 -0300

    Delete painel directory

--- painel/index-abc123.js
+++
@@ -1,2 +0,0 @@
-
-import{j as e,a as t}from"./vendor-abc123.js";function n(){return e.jsx("h1",{children:"Painel com login"})}t.createRoot(document.getElementById("root")).render(e.jsx(n,{}));
--- painel/index.js
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-function Painel() {
-  return React.createElement("h1", null, "Painel OrçaFácil carregado com sucesso!");
-}
-
-createRoot(document.getElementById("root")).render(React.createElement(Painel));
